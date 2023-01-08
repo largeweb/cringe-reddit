d1d13dad7f7a90994aa0adaa940f8438f6c2cc6f
matt <mat@unc.edu>
2023-01-08 00:30:06 -0500

    test

--- src/App.js
+++ src/App.js
@@ -5,8 +5,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div style={{position:"fixed",backgroundSize:"cover",backgroundPosition:"center",backgroundRepeat:"no-repeat",backgroundImage:`url(${bg})`,top:"0px",left:"0px",right:"0px",bottom:"0px"}}></div>
-      <div style={{top:"50%",left:"20%",width:"50%",background:"red"}}>hello</div>
+      <div style={{position:"fixed",backgroundSize:"cover",backgroundPosition:"center",backgroundRepeat:"no-repeat",backgroundImage:`url(${bg})`,top:"0px",left:"0px",right:"0px",zIndex:"-1",bottom:"0px"}}></div>
+      <div style={{position:"fixed",opacity:"0.3",top:"55%",left:"22.8%",width:"36%",height:"31%",zIndex:"1",background:"red"}}>hello</div>
+      <div style={{position:"fixed",opacity:"0.3",top:"22.6%",left:"3.6%",width:"17.8%",height:"77.4%",zIndex:"1",background:"red"}}>hello</div>
+      <div style={{position:"fixed",opacity:"0.3",top:"50.6%",left:"60.2%",width:"20.9%",height:"38.5%",zIndex:"1",background:"red"}}>hello</div>
+      <div style={{position:"fixed",opacity:"0.3",top:"18.4%",left:"60.2%",width:"20.9%",height:"29.8%",zIndex:"1",background:"red"}}>hello</div>
     </div>
   );
 }
