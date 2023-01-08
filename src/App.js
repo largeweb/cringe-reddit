1d8916e7789ca9630718a11b4516bfca7b5126e5
matt <mat@unc.edu>
2023-01-08 00:18:17 -0500

    test

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   return (
     <div className="App">
       <div style={{position:"fixed",backgroundSize:"cover",backgroundPosition:"center",backgroundRepeat:"no-repeat",backgroundImage:`url(${bg})`,top:"0px",left:"0px",right:"0px",bottom:"0px"}}></div>
+      <div style={{top:"50%",left:"20%",width:"50%",background:"red"}}>hello</div>
     </div>
   );
 }
